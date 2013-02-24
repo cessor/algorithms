c682056bbc370312b4722818061320960382521e
Johannes Hofmeister <cessor@github.com>
2013-02-24 20:48:32 +0100

    Finalized weighted union find. Added whitty comment.

--- quickunionweighted.py
+++ quickunionweighted.py
@@ -12,9 +12,10 @@ class QuickUnionWeighted(object):
 		return self.root(self.nodes[element])
 
 	def union(self, a, b):
+		''' *) '''
 		left = self.root(a)
 		right = self.root(b)
-		
+
 		if self.sizes[left] < self.sizes[right]:
 			child,parent = left,right
 		else:
@@ -26,3 +27,18 @@ class QuickUnionWeighted(object):
 		parent = point_to
 		self.sizes[parent] += self.sizes[child]
 		self.nodes[child] = parent
+
+
+
+'''
+*)
+
+# This is the code people want me to write, but I can't 
+int i = root(p);
+int j = root(q)
+
+if(sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i];}
+else  			  { id[j] = j; sz[i] += sz[j];}
+
+To me this reads like SZSZSZZSZSZSZIISZISIZSIZISZISIZISSIJSJZISJZIJSISJZISJ. 
+'''
