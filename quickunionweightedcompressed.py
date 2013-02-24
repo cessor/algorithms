ab62a7e889a51dac3e8761c808d78a9c4191ad97
Johannes Hofmeister <cessor@github.com>
2013-02-24 21:09:08 +0100

    Added simple one pass path compression (processed element points to its node)

--- quickunionweightedcompressed.py
+++ quickunionweightedcompressed.py
@@ -1,4 +1,4 @@
-class QuickUnionWeighted(object):
+class QuickUnionWeightedCompressed(object):
 	def __init__(self, elements):
 		self.nodes = range(elements)
 		self.sizes = [1] * elements
@@ -9,34 +9,22 @@ class QuickUnionWeighted(object):
 	def root(self, element):
 		if self.nodes[element] == element:
 			return element
-		return self.root(self.nodes[element])
+		return self.root(self.compress(element))
+
+	def compress(self, element):
+		self.nodes[element] = self.nodes[self.nodes[element]] 
+		return self.nodes[element]
 
 	def union(self, a, b):
-		''' *) '''
 		left = self.root(a)
 		right = self.root(b)
-
 		if self.sizes[left] < self.sizes[right]:
 			child,parent = left,right
 		else:
 			child,parent = right,left
-
 		self.make(child, point_to = parent)
 
 	def make(self, child, point_to):
 		parent = point_to
 		self.sizes[parent] += self.sizes[child]
 		self.nodes[child] = parent
-
-'''
-*)
-
-# This is the code people want me to write, but I can't 
-int i = root(p);
-int j = root(q)
-
-if(sz[i] < sz[j]) { id[i] = j; sz[j] += sz[i];}
-else  			  { id[j] = j; sz[i] += sz[j];}
-
-To me this reads like SZSZSZZSZSZSZIISZISIZSIZISZISIZISSIJSJZISJZIJSISJZISJ. 
-'''
