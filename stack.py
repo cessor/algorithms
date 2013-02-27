f3e733ac68f392696851cf142ba90ae743eeb72f
Johannes Hofmeister <cessor@github.com>
2013-02-27 02:53:44 +0100

    Added an implementation of a stack using a linked list

---
+++ stack.py
@@ -0,0 +1,14 @@
+class Stack(object):
+	def __init__(self):
+		self.head = None
+
+	def push(self, item):
+		self.head = (item, self.head)
+
+	def pop(self):
+		value,pointer = self.head
+		self.head = pointer
+		return value
+
+	def is_empty(self):
+		return self.head == None
---
+++ tests/test_list.py
@@ -0,0 +1,40 @@
+from nose.tools import *
+from stack import Stack
+
+Stack = Stack
+
+def test_new_stack_is_empty():
+	s = Stack()
+	assert_true(s.is_empty())
+
+def test_an_element_can_be_pushed_on_the_stack():
+	s = Stack()
+	s.push(1)
+	assert_false(s.is_empty())
+
+def test_two_elements_can_be_pushed_on_the_stack():
+	s = Stack()
+	s.push(1)
+	s.push(2)
+	assert_equal(s.pop(), 2)
+	assert_equal(s.pop(), 1)
+	assert_true(s.is_empty())
+
+def test_an_element_can_be_popped_from_the_stack():
+	s = Stack()
+	s.push(5)
+	x = s.pop()
+	assert_equal(x, 5)
+
+def test_lines_from_course():
+	s = Stack()	
+	buffer = []
+	dsl = 'to be or not to - be - - that - - - is'.split()
+	for word in dsl:
+		if word == '-':
+			buffer.append(s.pop())
+		else:
+			s.push(word)
+	assert_equal(' '.join(buffer), 'to be not that or be')
+	# Debug, use nosetests --nocapture to print...
+	print s.head
