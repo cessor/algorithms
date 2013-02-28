d67b35c980c0ae2ab1ca5dbd6e5ee56e42d2bca0
Cessor <cessor@github.com>
2013-02-28 14:46:15 +0100

    Even less code for the list stack implementation

--- liststack.py
+++ liststack.py
@@ -2,13 +2,12 @@ class ListStack(object):
 	def __init__(self):
 		self.head = None
 
-	def push(self, item):
-		self.head = (item, self.head)
+	def push(self, value):
+		self.head = (value, self.head)
 
 	def pop(self):
 		if self.is_empty(): return None
-		value,pointer = self.head
-		self.head = pointer
+		value,self.head = self.head
 		return value
 
 	def is_empty(self):
