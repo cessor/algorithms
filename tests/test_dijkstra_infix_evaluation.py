516642406a9b8053bfc7e43c3fe7ca9a9f0a808f
Johannes Hofmeister <cessor@github.com>
2013-03-02 20:16:01 +0100

    Implemented dijkstras algorithm

---
+++ tests/test_dijkstra_infix_evaluation.py
@@ -0,0 +1,57 @@
+from nose.tools import *
+import sys
+from arraystack import ArrayStack
+from dijkstraseval import dijkstrasAlgorithm
+
+Stack = ArrayStack
+
+def test_addition():
+	code = '(1 + 1)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 2)
+
+def test_dijkstra_more():
+	code = '(2 + 2)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 4)
+
+def test_multiply():
+	code = '(2 * 6)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 12)
+
+def test_substract():
+	code = '(2 - 2)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 0)
+
+def test_substract_order_positive():
+	code = '(4 - 2)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 2)
+
+def test_substract_order_negative():
+	code = '(2 - 4)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, -2)
+
+def test_divide():
+	code = '(2 / 2)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 1)
+
+def test_mod():
+	code = '(2 % 2)'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 0)
+
+@raises(Exception)
+def test_mod():
+	code = '(2 % 2) g'
+	result = dijkstrasAlgorithm(code)
+	result(assert_raises, 0)
+
+def test_items_from_course():
+	code = '(1 + (2 + 3) * (4 * 5)))'
+	result = dijkstrasAlgorithm(code)
+	assert_equal(result, 101)
