40eccf7020fe83927b626b65925937445c528b55
Johannes Hofmeister <cessor@github.com>
2013-03-02 20:26:27 +0100

    The errormessage works besser now

--- dijkstraseval.py
+++ dijkstraseval.py
@@ -24,14 +24,14 @@ def dijkstrasAlgorithm(str):
 			result = map[operators.pop()](a,b)
 			values.push(result)
 		elif c not in '( ':
-			arrow_to_show_error = ''.join((str.index(c) - 1) * ['-'])
-			message = 'Dafuq did I just read?\n%s\n%s^' % (str, arrow_to_show_error)
-			raise Exception(message, c)
+			arrow_to_show_error = ''.join((str.index(c)) * ['-'])
+			message = "Dafuq did I just read?\n%s\n%s^" % (str, arrow_to_show_error)
+			raise Exception(message)
 	return values.pop()
 
 if __name__ == '__main__':
 	try:
 		input = ' '.join(sys.argv[1:])
 		print dijkstrasAlgorithm(input)
-	except Exception as e: 
-		print e.message
+	except Exception as e:
+		print e
